90521b4f45d73687bc8f998664ec8202258b9843
Andreas Schroeder <andreas@a-netz.de>
2016-04-05 06:55:48 +0800

    Changed script to use class structure. Added command-line parameter to specify config file.

--- email-win-updates.py
+++ email-win-updates.py
@@ -1,128 +1,176 @@
 '''Script to check for windows updates and send mail with results.
 
-Modify the cfg dictionary below to configure the script.'''
+Copy the cfg_template.yaml to cfg.yaml and adapt the file contents to
+your needs.
+
+'''
 
 
 __docformat__ = 'ReStructuredText'
 __author__ = 'Andreas Schroeder <andreas@a-netz.de>'
 
 
+import argparse
 from email.mime.text import MIMEText
 import logging
 import os
 import smtplib
+import sys
 import textwrap
 import win32com.client as win32
 import yaml
 
 
-cfg = None
 
-def get_updates(search_crit):
-    '''Get a list of available windows updates.'''
-    
-    updateSession = win32.gencache.EnsureDispatch("Microsoft.Update.Session")
-    updateSearcher = updateSession.CreateUpdateSearcher()
-    
-    searchResult = updateSearcher.Search(search_crit)
+class EmailWinUpdates():
+    def __init__(self):
+        self.cfg = None
 
-    return searchResult.Updates
-
-
-def send_no_updates_mail():
-    '''Send mail with info about no available updates.'''
-    
-    body = 'Checking for updates was successful. \n' \
-           'There are currently no updates to install.'
-
-    send_mail(body)
-
-
-def send_updates_list_mail(updates):
-    '''Send mail with list of available updates.'''
-    
-    body = 'The following windows updates are available:\n\n'
-
-    for update in updates:
-        body += '* {sev]: {title}\n'.format(
-            sev=update.MsrcSeverity.upper(),
-            title=update.Title)
-        body += textwrap.fill(
-            update.Description,
-            width=70,
-            initial_indent='  ',
-            subsequent_indent='  ')
-        body += '\n\n'
-
-    send_mail(body)
-
-    
-def send_update_check_error_mail(ex):
-    '''Send mail with update check error info.'''
-    
-    body = 'Failed to check for windows updates:\n'
-    body += str(ex)
-    
-    send_mail(body)
-    
-
-def send_mail(body):
-    '''Compose and send the info mail.'''
-    
-    global cfg
-    
-    title = 'Windows Update check for {dom}\{comp}\n\n'.format(
-        dom=os.getenv('USERDOMAIN'),
-        comp=os.getenv('COMPUTERNAME'))
-
-    text = title
-    text += body
-    text += '\n\nSee http://github.com/motlib/email-win-updates for more info.'
-    
-    msg = MIMEText(text)
-
-    msg['Subject'] = title
-    msg['From'] = cfg['from']
-    msg['To'] = cfg['to']
-
-    s = smtplib.SMTP(cfg['server'], port=cfg['port'])
-    s.send_message(msg)
-    s.quit()
-    
-
-def main():
-    '''Program entry point. 
-
-    Search for updates and send out mail with result.'''
-    
-    global cfg
-
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s %(levelname)s: win_update_check: %(message)s')
-    
-    logging.info('Searching for updates...')
-
-    try:
-        with open('cfg.yaml', 'r') as f:
-            cfg = yaml.load(f)
         
-        # Check for updates
-        updates = get_updates(cfg['search_criteria'])
+    def get_updates(self, search_crit):
+        '''Get a list of available windows updates.
 
-        if len(updates) == 0 and cfg['no_updates_mail']:
-            # Send mail to tell about no updates available.
-            send_no_updates_mail()
-        else:
-            # send list of updates
-            send_updates_list_mail(updates)
+        Use win32com to create an instance of the
+        Microsoft.Update.Session COM object and query it for available
+        updates.
+
+        @param string search_crit: The search criteria for
+          updates. Default is `IsInstalled=0 and IsHidden=0`.
+
+        @returns: A list of update objects.
+
+        '''
+        
+        updateSession = win32.gencache.EnsureDispatch("Microsoft.Update.Session")
+        updateSearcher = updateSession.CreateUpdateSearcher()
+        
+        searchResult = updateSearcher.Search(search_crit)
+    
+        return searchResult.Updates
+    
+    
+    def send_no_updates_mail(self):
+        '''Send mail with info about no available updates.'''
+        
+        body = 'Checking for updates was successful. \n' \
+               'There are currently no updates to install.'
+    
+        self.send_mail(body)
+    
+    
+    def send_updates_list_mail(self, updates):
+        '''Send mail with list of available updates.'''
+        
+        body = 'The following windows updates are available:\n\n'
+    
+        for update in updates:
+            body += '* {sev]: {title}\n'.format(
+                sev=update.MsrcSeverity.upper(),
+                title=update.Title)
+            body += textwrap.fill(
+                update.Description,
+                width=70,
+                initial_indent='  ',
+                subsequent_indent='  ')
+            body += '\n\n'
+    
+        self.send_mail(body)
+    
+        
+    def send_update_check_error_mail(self, ex):
+        '''Send mail with update check error info.'''
+        
+        body = 'Failed to check for windows updates:\n'
+        body += str(ex)
+        
+        self.send_mail(body)
+        
+    
+    def send_mail(self, body):
+        '''Compose and send the info mail.'''
+        
+        title = 'Windows Update check for {dom}\{comp}\n\n'.format(
+            dom=os.getenv('USERDOMAIN'),
+            comp=os.getenv('COMPUTERNAME'))
+    
+        text = title
+        text += body
+        text += '\n\nGenerated by email-win-updates.py See \n' \
+                'http://github.com/motlib/email-win-updates for more info.'
+        
+        msg = MIMEText(text)
+    
+        msg['Subject'] = title
+        msg['From'] = self.cfg['from']
+        msg['To'] = self.cfg['to']
+    
+        s = smtplib.SMTP(self.cfg['server'], port=self.cfg['port'])
+        s.send_message(msg)
+        s.quit()
+
+
+    def parse_args(self):
+        '''Parse the command-line arguments.'''
+        
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument(
+            '--cfg',
+            help='Path to the config file.',
+            default='cfg.yaml')
+        
+        self.args = parser.parse_args()
+        
+        
+    def load_config(self):
+        '''Load the config file cfg.yaml from the current directory.'''
+        
+        try:
+            with open('cfg.yaml', 'r') as f:
+                self.cfg = yaml.load(f)
+        except:
+            # If we cannot open the config, we cannot send a mail to tell
+            # about if. So just log it and give up.
+            logging.exception('Failed to open config file.')
             
-    except Exception as ex:
-        send_update_check_error_mail(ex)
-        logging.exception('Failed to check for windows updates')
-
-    logging.info('Check for updates completed.')
-    
+            sys.exit(1)
         
+        
+    def run(self):
+        '''Program entry point. 
+    
+        Search for updates and send out mail with result.'''
+        
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s %(levelname)s: email-win-updates: %(message)s')
+        
+        logging.info('Searching for updates...')
+
+        self.parse_args()
+        
+        self.load_config()
+        
+        try:
+            # Check for updates
+            updates = self.get_updates(self.cfg['search_criteria'])
+    
+            if len(updates) == 0 and self.cfg['no_updates_mail']:
+                # Send mail to tell about no updates available.
+                self.send_no_updates_mail()
+            else:
+                # send list of updates
+                self.send_updates_list_mail(updates)
+                
+        except Exception as ex:
+            self.send_update_check_error_mail(ex)
+            logging.exception('Failed to check for windows updates')
+            
+            sys.exit(1)
+    
+        logging.info('Check for updates completed.')
+        
+            
 if __name__ == '__main__':
-    main()
+    ewu = EmailWinUpdates()
+    ewu.run()
